3b15611d76a51dba6670d1b7097a9b72b6a25c5d
Fahad Siddiqui <fahadsidd21@gmail.com>
2026-08-06 03:22:04 +0530

    pushing components example

--- react-project1/src/App.jsx
+++ react-project1/src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
+import Card from './components/Card';
 
 const App = () => {
   return (
-    <>
-      <div className = "abc">
-        <h1>Welcome to React Project 1</h1>
-      </div>
-      <div className = "xyz">
-        <h2>This is a sample React application.</h2>
-      </div>
-    </>
+    <div>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
   )
 }
 
---
+++ react-project1/src/components/Card.jsx
@@ -0,0 +1,10 @@
+const Card = () => {
+    return (
+        <div className="card">
+            <h3>Card Title</h3>
+            <p>This is a simple card component.</p>
+        </div>
+    )
+}
+
+export default Card;
