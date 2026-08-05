2377ca4eb8cc4245884757a47238e04aad36ab90
Fahad Siddiqui <fahadsidd21@gmail.com>
2026-08-06 02:59:39 +0530

    pushing fragments example

---
+++ react-project1/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <div className = "abc">
+        <h1>Welcome to React Project 1</h1>
+      </div>
+      <div className = "xyz">
+        <h2>This is a sample React application.</h2>
+      </div>
+    </>
+  )
+}
+
+export default App;
